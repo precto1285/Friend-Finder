2ab6f20c5cfef058df19e48290260d56e9039ce0
Philippe Recto <precto1285@gmal.com>
2018-04-22 14:33:34 -0700

    app.get htmlRoute.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,8 @@
+// Your `htmlRoutes.js` file should include two routes:
+
+//    * A GET Route to `/survey` which should display the survey page.
+//    * A default, catch-all route that leads to `home.html` which displays the home page. 
+
+app.get('/survey', function(request, response) {
+    response.sendFile(__drname + '../public/survey.html');
+});
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-var express = require('express');
-var bodyParder = require('body-parser');
-var path = require('path');
+// Your `server.js` file should require the basic 
+// npm packages we've used in class: `express`, `body-parser` and `path`.
+
+const express = require('express');
+const bodyParder = require('body-parser');
+const path = require('path');
