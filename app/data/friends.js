ba2ced21a93eefce3bf47caf0bf2a92ca968696a
Philippe Recto <precto1285@gmal.com>
2018-04-24 17:27:26 -0700

    added info for array

--- app/data/friends.js
+++ app/data/friends.js
@@ -31,53 +31,63 @@
 // Your app should calculate both 5-3 and 3-5 as 2, and so on.
 // // The closest match will be the user with the least amount of difference.
 
+
+var user1='';
+var user2='';
+
+
+var friendsArray = [
 {
     "name" : "Booger",
     "photo" : "images/booger.jpg",
     "scores" : [1,2,3,4,5,1,2,3,4,5]
-}
+},
 {
     "name":"Shiner",
     "photo":"images/shine.jpg",
     "scores":[2,3,4,5,1,2,3,4,5,1]
-}
+},
 {
     "name":"Cassy",
     "photo":"images/cassy.jpg",
     "scores":[3,4,5,1,2,3,4,5,1,2]
-}
+},
 {
     "name":"Snape",
     "photo":"images/snapes.jpg",
     "scores":[4,5,1,2,3,4,5,1,2,3]
-}
+},
 {
     "name":"Clarice",
     "photo":"images/clarice.jpg",
     "scores":[5,1,2,3,4,5,1,2,3,4]
-}
+},
 {
     "name":"Stark",
     "photo":"images/stark.jpg",
     "scores":[5,4,3,2,1,5,4,3,2,1]
-}
+},
 {
     "name":"Hal",
     "photo":"images/hal.jpg",
     "scores":[4,3,2,1,5,4,3,2,1,5]
-}
+},
 {
     "name":"Chloe",
     "photo":"images/chloe.jpg",
     "scores":[3,2,1,5,4,3,2,1,5,4]
-}
+},
 {
     "name":"Charles",
     "photo":"images/charles.jpg",
     "scores":[2,1,5,4,3,2,1,5,4,3]
-}
+},
 {
     "name":"Dingo",
     "photo":"images/dingo.jpg",
     "scores":[1,5,4,3,2,1,5,4,3,2]
-}
+}];
+
+for(var i = 0; i < friendsArray.length; i++){
+    console.log(friendsArray[i]);
+};
